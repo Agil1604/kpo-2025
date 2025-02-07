183c2b5048571b969566bc23a11bfa6920dbfa1c
Agil1604 <asamirov@edu.hse.ru>
2025-02-07 21:05:57 +0300

    did the task

--- practice/practise-4/src/test/java/hse/kpo/KpoApplicationTests.java
+++ practice/practise-4/src/test/java/hse/kpo/KpoApplicationTests.java
@@ -2,6 +2,7 @@ package hse.kpo;
 
 import hse.kpo.domains.Customer;
 import hse.kpo.factories.HandCarFactory;
+import hse.kpo.factories.LevitatingCarFactory;
 import hse.kpo.factories.PedalCarFactory;
 import hse.kpo.params.EmptyEngineParams;
 import hse.kpo.params.PedalEngineParams;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -32,6 +34,9 @@ class KpoApplicationTests {
 	@Autowired
 	private HandCarFactory handCarFactory;
 
+	@Autowired
+	private LevitatingCarFactory levitatingCarFactory;
+
 	@Test
 	@DisplayName("Тест загрузки контекста")
 	void contextLoads() {
@@ -43,11 +48,13 @@ class KpoApplicationTests {
 	@Test
 	@DisplayName("Тест загрузки контекста")
 	void hseCarServiceTest() {
-		customerStorage.addCustomer(new Customer("Ivan1",6,4));
-		customerStorage.addCustomer(new Customer("Maksim",4,6));
-		customerStorage.addCustomer(new Customer("Petya",6,6));
-		customerStorage.addCustomer(new Customer("Nikita",4,4));
-		
+		customerStorage.addCustomer(new Customer("Ivan",6,4, 500));
+		customerStorage.addCustomer(new Customer("Nikita",4,4, 125));
+		customerStorage.addCustomer(new Customer("Maksim",4,6, 163));
+		customerStorage.addCustomer(new Customer("Petya",6,6, 25));
+
+		carService.addCar(levitatingCarFactory, EmptyEngineParams.DEFAULT);
+
 		carService.addCar(pedalCarFactory, new PedalEngineParams(6));
 		carService.addCar(pedalCarFactory, new PedalEngineParams(6));
 
@@ -61,4 +68,9 @@ class KpoApplicationTests {
 		customerStorage.getCustomers().stream().map(Customer::toString).forEach(System.out::println);
 	}
 
+	@Test
+	@DisplayName("Тест добавления пользователя")
+	void CarServiceTest(){
+
+	}
 }
